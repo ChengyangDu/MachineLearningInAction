34c366a15336d0a22a0cbfaac922a36a5371375e
Ducy <chengyang.du@outlook.com>
2018-08-14 23:10:45 -0700

    Add implementations for classifying dating data

--- kNN.py
+++ kNN.py
@@ -22,6 +22,43 @@ def classify0 (inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)
     return sortedClassCount[0][0]
 
+def file2Matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
 
-    group, labels = createDataSet()
-    print (classify0([0,0], group, labels, 3))
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+
+    ranges = maxVals-minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest(filename):
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2Matrix(filename)
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m],3)
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1
+    print (errorCount/float(numTestVecs))
