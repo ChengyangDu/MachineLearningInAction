cebcf27ccd00a1c236aad9bc25d8dd855b81e9ee
ChengyangDu <chengyang.du@outlook.com>
2018-08-15 21:36:05 +0800

    decision tree by entropy

---
+++ trees.py
@@ -0,0 +1,63 @@
+from math import log
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCount = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCount.keys():
+            labelCount[currentLabel] = 0
+        labelCount[currentLabel] += 1
+    shannonEnt = 0.0
+    
+    for key in labelCount:
+        prob = float(labelCount[key])/numEntries
+        shannonEnt -= prob*log(prob,2)
+    return shannonEnt
+
+
+def createDataSet():
+    dataSet = [[1,1,'yes'],
+            [1,1,'yes'],
+            [1,0,'no'],
+            [0,1,'no'],
+            [0,1,'no']]
+    labels = ['no surfacting', 'flippers']
+    return dataSet, labels
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            reducedFeatVec.append(reducedFeatVec)
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0])-1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInforGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featureList = [example[i] for example in dataSet]
+        uniqueVals = set(featureList)
+        
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+            #total Entropy = prob of one case in one feature * Entropy of samples who have this case in this feature
+        
+        infoGain = baseEntropy - newEntropy
+
+        if(infoGain>bestInforGain):
+    
+            bestInforGain = infoGain
+            bestFeature = i
+    
+    return bestFeature
+
+
+
